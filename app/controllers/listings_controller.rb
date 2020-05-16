6e0e8670d4713aed78e6fa5efefa0ff4a70a0f74
Oliver Mason <omason@protonmail.com>
2020-05-17 00:01:04 +1000

    Listings form submittable

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -11,4 +11,16 @@ class ListingsController < ApplicationController
         @lams = Listing.lams.keys
     end
 
+    def create
+        @listing = current_user.listings.create(listing_parameters)
+        puts @listing.errors.full_messages  
+        redirect_to listings_path
+        # render json: params
+    end
+
+    private
+
+    def listing_parameters
+        params.require(:listing).permit(:title, :manufacturer, :model, :style, :price, :location, :mileage, :lams, :description, :engine, :year, :picture)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get "/", to: "pages#index", as: "root"
   get "/listings/", to: "listings#index", as: "listings"
   get "/listings/new", to: "listings#new", as: "new_listing"
-
+  post "/listings/", to: "listings#create"
 end
