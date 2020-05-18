fb40f3b302624d8cb3121f59e624f0bc435d58d4
Oliver Mason <omason@protonmail.com>
2020-05-18 12:31:42 +1000

    Create Manufacturer entity

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,5 +2,6 @@ class Listing < ApplicationRecord
   belongs_to :user
   enum lams: { Yes: 0, No: 1 }
   validates :title, :manufacturer, :model, :style, :price, :location, :mileage, :lams, :description, :engine, :year, presence: true
+  has_one :manufacturer
   has_one_attached :picture
 end
