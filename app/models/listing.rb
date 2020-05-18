bcac74738de2320503768c101779ca43401a2034
Oliver Mason <omason@protonmail.com>
2020-05-18 14:25:25 +1000

    Change Model entity for style entity, and create seed files

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,7 @@ class Listing < ApplicationRecord
   enum lams: { Yes: 0, No: 1 }
   validates :title, :manufacturer, :model, :style, :price, :location, :mileage, :lams, :description, :engine, :year, presence: true
   has_one :manufacturer
-  has_one :model
+  has_one :style
   has_one :location
   has_one_attached :picture
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_023512) do
+ActiveRecord::Schema.define(version: 2020_05_18_041859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,12 @@ ActiveRecord::Schema.define(version: 2020_05_18_023512) do
 
   create_table "models", force: :cascade do |t|
     t.string "model"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.string "style"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
