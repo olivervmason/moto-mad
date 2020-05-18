217ac34eebc2eb4b0527ce3a6533b32ef1b9153a
Oliver Mason <omason@protonmail.com>
2020-05-18 12:35:55 +1000

    Add bike model entity

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,6 @@ class Listing < ApplicationRecord
   validates :title, :manufacturer, :model, :style, :price, :location, :mileage, :lams, :description, :engine, :year, presence: true
   has_one :manufacturer
   has_one :model
+  has_one :location
   has_one_attached :picture
 end
