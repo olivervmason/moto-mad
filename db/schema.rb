9b0221ea6a1da792bc4e0cb2c53dc95e165f680f
Oliver Mason <omason@protonmail.com>
2020-05-19 10:07:09 +1000

    Add Answer model and update Q&A models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_060802) do
+ActiveRecord::Schema.define(version: 2020_05_19_000425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2020_05_18_060802) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "answers", force: :cascade do |t|
+    t.bigint "question_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "listings", force: :cascade do |t|
@@ -75,6 +83,15 @@ ActiveRecord::Schema.define(version: 2020_05_18_060802) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.bigint "listing_id"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_questions_on_listing_id"
+  end
+
   create_table "styles", force: :cascade do |t|
     t.string "style"
     t.datetime "created_at", null: false
@@ -94,8 +111,10 @@ ActiveRecord::Schema.define(version: 2020_05_18_060802) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "answers", "questions"
   add_foreign_key "listings", "locations"
   add_foreign_key "listings", "manufacturers"
   add_foreign_key "listings", "styles"
   add_foreign_key "listings", "users"
+  add_foreign_key "questions", "listings"
 end
